bdf0450ef11f57ed11ec42cc8d3beb7d63c926b5
cquest <cquest@openstreetmap.fr>
2018-01-29 12:05:27 +0100

    ajout ID unique

--- batch.sh
+++ batch.sh
@@ -19,16 +19,16 @@ psql -c "
 
 -- table de description des DFI
 DROP TABLE IF EXISTS dgfip_dfi;
-CREATE TABLE dgfip_dfi (depcom char(5), id_dfi char(7), nature_dfi char(1), validation char(8), lot char(5), meres text[], filles text[]);
+CREATE TABLE dgfip_dfi (id text, depcom char(5), id_dfi char(7), nature_dfi char(1), validation char(8), lot char(5), meres text[], filles text[]);
 
 -- création des DFI et parcelles 'mères'
-INSERT INTO dgfip_dfi SELECT left(raw,2)||substr(raw,5,3) as depcom, substr(raw,13,7) as id_dfi, substr(raw,21,1) as nature_dfi, substr(raw,23,8) as validation, substr(raw,63,5) as lot, string_to_array(substr(replace(replace(left(substr(raw,70),-1),' ','0'),';',';'||left(raw,2)||substr(raw,5,3)||substr(raw,9,3)),2),';') as meres FROM dgfip_dfi_raw WHERE substr(raw,69,1)='1';
+INSERT INTO dgfip_dfi SELECT replace(left(raw,20)||substr(raw,63,5),';','') as id, left(raw,2)||substr(raw,5,3) as depcom, substr(raw,13,7) as id_dfi, substr(raw,21,1) as nature_dfi, substr(raw,23,8) as validation, substr(raw,63,5) as lot, string_to_array(substr(replace(replace(left(substr(raw,70),-1),' ','0'),';',';'||left(raw,2)||substr(raw,5,3)||substr(raw,9,3)),2),';') as meres FROM dgfip_dfi_raw WHERE substr(raw,69,1)='1';
 
--- index sur code commune, dfi et lot (en principe unique)
-CREATE INDEX dgfip_dfi_unique ON dgfip_dfi (depcom,id_dfi,lot);
+-- index sur code commune, préfixe, dfi et lot (en principe unique)
+CREATE INDEX dgfip_dfi_id ON dgfip_dfi (id);
 
 -- ajout des parcelles 'mères'
-WITH u AS (SELECT left(raw,2)||substr(raw,5,3) as u_depcom, substr(raw,13,7) as u_id_dfi, substr(raw,63,5) as u_lot, string_to_array(substr(replace(replace(left(substr(raw,70),-1),' ','0'),';',';'||left(raw,2)||substr(raw,5,3)||substr(raw,9,3)),2),';') as parcelles FROM dgfip_dfi_raw WHERE substr(raw,69,1)='2') UPDATE dgfip_dfi SET filles=parcelles from u WHERE depcom=u_depcom AND id_dfi=u_id_dfi and lot = u_lot;
+WITH u AS (SELECT replace(left(raw,20)||substr(raw,63,5),';','') as u_id, string_to_array(substr(replace(replace(left(substr(raw,70),-1),' ','0'),';',';'||left(raw,2)||substr(raw,5,3)||substr(raw,9,3)),2),';') as parcelles FROM dgfip_dfi_raw WHERE substr(raw,69,1)='2') UPDATE dgfip_dfi SET filles=parcelles from u WHERE id=u_id;
 
 -- index sur les parcelles mères et filles
 CREATE INDEX dgfip_dfi_meres ON dgfip_dfi USING GIN (meres);
@@ -39,7 +39,7 @@ CREATE INDEX dgfip_dfi_filles ON dgfip_dfi USING GIN (filles);
 "
 
 # export JSON
-psql -c "\copy (select json_build_object('depcom',depcom, 'id_dfi',id_dfi, 'lot_dfi',lot, 'nature',nature_dfi, 'date',validation::date, 'meres',meres, 'filles',filles) from dgfip_dfi) to dfi.json;"
+psql -c "\copy (select json_build_object('id', id, 'depcom',depcom, 'id_dfi',id_dfi, 'lot_dfi',lot, 'nature',nature_dfi, 'date',validation::date, 'meres',meres, 'filles',filles) from dgfip_dfi) to dfi.json;"
 
 # export postgresql
 pg_dump -t dgfip_dfi -o -O -x > dfi.sql
@@ -47,4 +47,3 @@ pg_dump -t dgfip_dfi -o -O -x > dfi.sql
 # compression gzip
 gzip -9 dfi.sql
 gzip -9 dfi.json
-
